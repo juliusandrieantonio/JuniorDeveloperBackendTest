a2f251da7a60d7eb109767a30e7f782e1c541105
Julius Andrie Antonio <127472421+PeleboyKulot03@users.noreply.github.com>
2024-07-27 16:13:10 +0800

    first commit

---
+++ src/main/java/org/example/ui/DataGetter.java
@@ -0,0 +1,102 @@
+package org.example.ui;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.example.models.RandomDataModel;
+import org.example.utils.SingletonInstance;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class DataGetter {
+    private int num;
+    private String format;
+    private ObjectMapper objectMapper;
+
+
+    // constructor to get the input and format that the user want
+    public DataGetter(int num, String format) {
+        SingletonInstance singletonInstance = SingletonInstance.getInstance();
+        objectMapper = singletonInstance.getObjectMapper();
+        this.num = num;
+        this.format = format;
+
+    }
+
+    // class to get the data
+    public void getData() throws IOException, InterruptedException {
+        int i = 0;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://bored-api.appbrewery.com/random"))
+                .build();
+        switch (format) {
+            case "console":
+                while (i < num) {
+                    HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+                    RandomDataModel model = objectMapper.readValue(response.body(), RandomDataModel.class);
+                    System.out.println(model);
+                    i++;
+                }
+                return;
+
+            case "json":
+                while (i < num) {
+                    HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+                    RandomDataModel model = objectMapper.readValue(response.body(), RandomDataModel.class);
+
+                    String fileName = "test.json";
+                    File jsonFile = new File(fileName);
+                    int tempCounter = 0;
+                    while(jsonFile.exists()) {
+                        tempCounter++;
+                        fileName = "test(" + tempCounter + ").json";
+                        jsonFile = new File(fileName);
+                    }
+
+                    objectMapper.writeValue(jsonFile, model);
+                    System.out.println("JSON file downloaded in: " + jsonFile.getAbsoluteFile());
+                    i++;
+                }
+                return;
+
+            case "csv":
+                while (i < num) {
+                    HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+                    RandomDataModel model = objectMapper.readValue(response.body(), RandomDataModel.class);
+                    CsvSchema.Builder csvSchemaBuilder = CsvSchema.builder();
+
+                    JsonNode jsonTree = objectMapper.valueToTree(model);
+
+                    jsonTree.fieldNames().forEachRemaining(csvSchemaBuilder::addColumn);
+                    CsvSchema csvSchema = csvSchemaBuilder.build().withHeader();
+                    CsvMapper csvMapper = new CsvMapper();
+
+                    String fileName = "test.csv";
+                    File csvFile = new File(fileName);
+                    int tempCounter = 0;
+                    while(csvFile.exists()) {
+                        tempCounter++;
+                        fileName = "test(" + tempCounter + ").csv";
+                        csvFile = new File(fileName);
+                    }
+
+                    csvMapper.writerFor(JsonNode.class)
+                            .with(csvSchema)
+                            .writeValue(csvFile, jsonTree);
+                    System.out.println("CSV file downloaded in: " + csvFile.getAbsoluteFile());
+                    i++;
+                }
+                return;
+
+            default:
+                System.out.println("Invalid input!");
+        }
+    }
+}
---
+++ src/main/java/org/example/ui/Main.java
@@ -0,0 +1,13 @@
+package org.example.ui;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        // Class utility to get the data from the API
+        // first argument is the number of data you want to get
+        // second argument is the type of data you want it to get sample (csv, json, console)
+        DataGetter dataGetter = new DataGetter(10, "test");
+        dataGetter.getData();
+    }
+}
